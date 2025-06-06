15e0b01850d8ad30b861625e6464c747a3528351
castro <154817786+ocastr0@users.noreply.github.com>
2025-06-06 16:37:03 -0300

    Delete controle-note directory

--- controle-note/api/stats.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *');
-
-require_once '../classes/NotebookSystem.php';
-
-$system = new NotebookSystem();
-echo json_encode($system->getEstatisticas());
-?>
